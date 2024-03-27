4b49171a5bf3cb9435c9f5a42ce00a32f241862b
ggenelot <77398544+ggenelot@users.noreply.github.com>
2024-03-27 12:27:02 +0100

    Manualy added links to model pages

--- web scrapping/script.py
+++ web scrapping/script.py
@@ -12,7 +12,62 @@ liste_url = ['https://www.iamconsortium.org/resources/model-resources/aim-hub/',
              'https://www.iamconsortium.org/resources/model-resources/cias-community-integrated-assessment-system/', 
              'https://www.iamconsortium.org/resources/model-resources/clews-models-assess-how-production-and-use-of-resources-affect-climate-change/', 
              'https://www.iamconsortium.org/resources/model-resources/computable-framework-for-energy-and-the-environment-model-coffee/', 
+
+
              'https://www.iamconsortium.org/resources/model-resources/dne21/', 
+             'https://www.iamconsortium.org/resources/model-resources/dsk-the-dystopian-schumpeter-meeting-keynes-climate-economy-agent-based-model/', 
+             'https://www.iamconsortium.org/resources/model-resources/e3me-ftt/', 
+             'https://www.iamconsortium.org/resources/model-resources/e3mg/', 
+             'https://www.iamconsortium.org/resources/model-resources/elena-ecuador-land-use-and-energy-network-analysis-model/', 
+             'https://www.iamconsortium.org/resources/model-resources/empire-european-model-for-power-system-investment-with-renewable-energy/', 
+             'https://www.iamconsortium.org/news-from-the-community/news-f-the-community/en-roads/', 
+             'https://www.iamconsortium.org/resources/model-resources/env-linkages-moodel/', 
+             'https://www.iamconsortium.org/resources/model-resources/envisage-environmental-impact-and-sustainability-applied-general-equilibrium-model/', 
+             'https://www.iamconsortium.org/resources/model-resources/eppa/', 
+
+             'https://www.iamconsortium.org/resources/model-resources/eucalc-european-calculator/', 
+             'https://www.iamconsortium.org/resources/model-resources/exiomod-2-0-extended-input-output-model/', 
+             'https://www.iamconsortium.org/resources/model-resources/fairv2-0-0-finite-amplitude-impulse-response-v2-0-0/',
+             'https://www.iamconsortium.org/resources/model-resources/fasom-ghg-forestry-and-agricultural-sector-optimization-model-with-greenhouse-gases/', 
+             'https://www.iamconsortium.org/resources/model-resources/felix/', 
+             'https://www.iamconsortium.org/resources/model-resources/fund-climate-framework-for-uncertainty-negotiation-and-distribution/', 
+             'https://www.iamconsortium.org/resources/model-resources/gains-model/', 
+             'https://www.iamconsortium.org/resources/model-resources/gcam/', 
+             'https://www.iamconsortium.org/resources/model-resources/gem-e3/', 
+             'https://www.iamconsortium.org/resources/model-resources/gemini-e3-eu/', 
+
+             'https://www.iamconsortium.org/resources/model-resources/get-global-energy-system-model/', 
+             'https://www.iamconsortium.org/resources/model-resources/globio-global-biodiversity-model-for-policy-support/', 
+             'https://www.iamconsortium.org/resources/model-resources/globiom-global-biosphere-management-model/', 
+             'https://www.iamconsortium.org/resources/model-resources/glucose/', 
+             'https://www.iamconsortium.org/resources/model-resources/grace/', 
+             'https://www.iamconsortium.org/resources/model-resources/ices/', 
+             'https://www.iamconsortium.org/resources/model-resources/ifs/', 
+             'https://www.iamconsortium.org/resources/model-resources/igsm-integrated-global-system-model/', 
+             'https://www.iamconsortium.org/resources/model-resources/imaclim-r/', 
+
+             'https://www.iamconsortium.org/resources/model-resources/nemesis-world/', 
+             'https://www.iamconsortium.org/resources/model-resources/omnia/', 
+             'https://www.iamconsortium.org/resources/model-resources/open-prom/', 
+             'https://www.iamconsortium.org/news-from-the-community/news-f-the-community/osemosys-open-source-energy-modelling-system/', 
+             'https://www.iamconsortium.org/resources/model-resources/oxford-global/', 
+             'https://www.iamconsortium.org/resources/model-resources/page2002-policy-analysis-of-the-greenhouse-effect/', 
+             'https://www.iamconsortium.org/news-from-the-community/news-f-the-community/plaia-plastics-integrated-assessment-model/', 
+             'https://www.iamconsortium.org/resources/model-resources/plan4res/', 
+             'https://www.iamconsortium.org/resources/model-resources/poles-prospective-outlook-on-long-term-energy-systems/', 
+             'https://www.iamconsortium.org/resources/model-resources/prometheus/', 
+
+             'https://www.iamconsortium.org/resources/model-resources/remes/', 
+             'https://www.iamconsortium.org/resources/model-resources/remind/', 
+             'https://www.iamconsortium.org/resources/model-resources/rev-the-renewable-energy-potential-model/', 
+             'https://www.iamconsortium.org/resources/rice50-a-multi-regional-integrated-assessment-model/', 
+             'https://www.iamconsortium.org/resources/model-resources/stem-swiss-times-energy-systems-model/', 
+             'https://www.iamconsortium.org/resources/model-resources/tempest-technological-economic-political-energy-systems-transition/', 
+             'https://www.iamconsortium.org/resources/model-resources/us-regen-u-s-regional-economy-greenhouse-gas-and-energy/', 
+             'https://www.iamconsortium.org/resources/model-resources/witch/'
+
+
+
              ]
 
 def metadonnees(url):
